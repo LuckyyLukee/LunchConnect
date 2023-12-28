c8bb8c2623754b9492cf0c038706e0ad6e62540e
Lucas Magnusson <lucasmyeet@gmail.com>
2023-12-28 15:02:38 +0100

    basic page setup Co-authored-by: Mohamed <mohamadalihassan99@outlook.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
+
 function App() {
-
-
   return (
     <>
+      <Header />
+      <main className="main-wrapper"></main>
+      <Footer />
     </>
   )
 }
---
+++ src/pages/NetworkPage/NetworkPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NetworkPage() {
+  return (
+    <div>NetworkPage</div>
+  )
+}
